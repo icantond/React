86621b26039720002d3328aabb7695be9549a17e
icantond <cantondebat@outlook.com>
2023-05-14 11:37:35 -0300

    Se quitaron comentarios innecesarios

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,4 +1,5 @@
 import ItemCount from "../ItemCount/ItemCount"
+import "./ItemDetail.css"
 
 const ItemDetail = ({product}) => {
 
@@ -6,16 +7,16 @@ const ItemDetail = ({product}) => {
 
         <div className="container-fluid">
             <article className="row">
-                <h2>{product.titulo}</h2>
-                <h2>{product.artista}</h2>
+                <h2 className="product-title">{product.titulo}</h2>
+                <h2 className="product-subtitle">{product.artista}</h2>
                 <div className="item-detail-img-container">
-                    <img src={product.imagen} alt={product.titulo}/>
+                    <img className="item-detail-img" src={product.imagen} alt={product.titulo}/>
                 </div>
-                <p>Año: {product.anio}</p>
-                <p>Precio: ${product.precio}</p>
-                <p><strong>Lista de canciones: </strong><br/> {product.tracklist}</p>
+                <p className="product-year">Año: {product.anio}</p>
+                <p className="product-price">Precio: <strong>${product.precio}</strong></p>
+                <p className="product-description"><strong>Lista de canciones: </strong><br/> {product.tracklist}</p>
                 <ItemCount initial={1} stock={product.stock} onAdd={(quantity) => console.log("Cantidad agregada ", quantity)}/>
-                <p> Código:{product.id}</p>
+                <p className="product-id"> Código:{product.id}</p>
             </article>
         </div>
     )
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -16,37 +16,6 @@ const NavBar = () => {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav">
-                        {/* <li className="nav-item dropdown">
-                            <NavLink className="nav-link dropdown-toggle" to role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Rock
-                            </NavLink>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Rock Nacional</a></li>
-                                <li><a className="dropdown-item" href="#">Rock Internacional</a></li>
-                            </ul>
-                        </li>
-
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Pop
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Nacional</a></li>
-                                <li><a className="dropdown-item" href="#">Internacional</a></li>
-                                <li><a className="dropdown-item" href="#">Latino</a></li>
-                            </ul>
-                        </li>
-
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Otros
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">R&B</a></li>
-                                <li><a className="dropdown-item" href="#">Brit Rock</a></li>
-                                <li><a className="dropdown-item" href="#">Electrónica</a></li>
-                            </ul>
-                        </li> */}
                         <NavLink to={'category/Rock Nacional'} className={({isActive}) => isActive ? 'ActiveOption' : 'Option nav-link'}>Rock Nacional</NavLink>
                         <NavLink to={'category/Rock Internacional'} className={({isActive}) => isActive ? 'ActiveOption' : 'Option nav-link'}>Rock Internacional</NavLink>
                         <NavLink to={'category/Pop Nacional'} className={({isActive}) => isActive ? 'ActiveOption' : 'Option nav-link'}>Pop Nacional</NavLink>
